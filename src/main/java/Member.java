88e15f95d7eab3029eb0d760054acf2800f74bea
MINSU <vhdhxh@naver.com>
2023-12-22 23:30:35 +0900

    setting

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,14 +1,28 @@
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Member {
-    private String student;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
 
-    public void setStudent(String student) {
-        this.student = student;
+    public Long getId() {
+        return id;
     }
 
-    public String getStudent() {
-        return student;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/jpamain.java
+++ src/main/java/jpamain.java
@@ -6,14 +6,28 @@ import javax.persistence.Persistence;
 public class jpamain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         Member member = new Member();
-        member.setStudent("studnet1");
-        tx.begin();
+       tx.begin();
+        member.setName("학생");
         em.persist(member);
+        Member member1 = em.find(Member.class,1L);
+
+        em.flush();
+
         tx.commit();
+        em.close();
+//        em.persist(member);
+//        Member student = em.find(Member.class , member.getName());
+
+
+
+//        Member member = new Member();
+//        member.setStudent("studnet1");
+//        tx.begin();
+//        em.persist(member);
+//        tx.commit();
 
 
     }
